9ac71732993c4e87f0f57d7c2d01c4e090e89200
caiotelles <caiotelles@gmail.com>
2020-01-08 19:47:40 -0300

    adicionando actions no core

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,3 +1,5 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
@@ -7,3 +9,29 @@ class PontoTuristicoViewSet(ModelViewSet):
 
         def get_queryset(self):
             return PontoTuristico.objects.filter(aprovado=True)
+
+#       def list(self, request, *args, **kwargs):
+#           return Response({'teste': 123})
+
+#       def create(self, request, *args, **kwargs):
+#           return Response({'Hello': request.data['nome']})
+
+#       def destroy(self, request, *args, **kwargs):
+#           pass
+
+#       def retrieve(self, request, *args, **kwargs):
+#           pass
+
+#       def update(self, request, *args, **kwargs):
+#           pass
+
+#       def partial_update(self, request, *args, **kwargs):
+#           pass
+
+        @action(methods=['GET', 'POST'], detail=True)
+        def denunciar(self, request, pk=None):
+            pass
+
+        @action(methods=['GET'], detail=False)
+        def teste(self, request):
+            pass
