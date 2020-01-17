88e7f69acec0db9cc8cceff3464ad95b47feebe5
caiotelles <caiotelles@gmail.com>
2020-01-16 23:01:36 -0300

    adicionando searchfield em pontosturisticos

--- atracoes/api/viewsets.py
+++ atracoes/api/viewsets.py
@@ -1,3 +1,4 @@
+#from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
 from atracoes.models import Recurso
 from .serializers import AtracaoSerializer
@@ -5,3 +6,5 @@ from .serializers import AtracaoSerializer
 class AtracoesViewSet(ModelViewSet):
     queryset = Recurso.objects.all()
     serializer_class = AtracaoSerializer
+#    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ['nome', 'descricao']
