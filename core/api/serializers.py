73d6fc5f06833d358e01efc2bb1bf8baf4c049d9
caiotelles <caiotelles@gmail.com>
2020-01-19 19:16:48 -0300

    adicionando objeto nestedrelationships no serializers de pontosturisticos

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -3,12 +3,14 @@ from core.models import PontoTuristico
 from atracoes.api.serializers import AtracaoSerializer
 from enderecos.api.serializers import EnderecoSerializer
 from comentarios.api.serializers import ComentarioSerializer
+from avaliacoes.api.serializers import AvaliacaoSerializer
 
 class PontoTuristicoSerializer(ModelSerializer):
 
     recursos = AtracaoSerializer(many=True)
     comentarios = ComentarioSerializer(many=True)
     endereco = EnderecoSerializer()
+    avaliacoes = AvaliacaoSerializer(many=True)
 
     class Meta:
         model = PontoTuristico
